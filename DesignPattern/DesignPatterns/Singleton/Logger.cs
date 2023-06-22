c51d26c9a4a3e9fa7def03122e4b1c6a514726d6
kmikurda@abis.krakow.pl <kmikurda@abis.krakow.pl>
2023-06-22 14:13:37 +0200

    add singleton

---
+++ DesignPattern/DesignPatterns/Singleton/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _logger = new Lazy<Logger>(() => new Logger()); //thread-safe way to init Singleton only once when first call occured
+        public static Logger Instance => _logger.Value;
+
+        protected Logger()
+        {
+            
+        }
+
+        public void Log(string Message)
+        {
+            Console.WriteLine($"Message: {Message}");
+        }
+    }
+}
---
+++ DesignPattern/DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var invalidLoggerInstant = new Logger();
+            var logger1 = Logger.Instance;
+            var logger2 = Logger.Instance;
+            if (logger2 == logger1)
+            {
+                logger2.Log("same instance");
+
+            }
+        }
+    }
+} 
