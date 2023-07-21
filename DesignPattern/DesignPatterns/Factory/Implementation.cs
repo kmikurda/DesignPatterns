01a5917fa7cda2e63962485aa1442d1d7bebd768
kmikurda@abis.krakow.pl <kmikurda@abis.krakow.pl>
2023-07-21 14:16:02 +0200

    Factory pattern

---
+++ DesignPattern/DesignPatterns/Factory/Implementation.cs
@@ -0,0 +1,67 @@
+﻿namespace Factory
+{
+    public abstract class DiscountService
+    {
+         public abstract int DiscountPercentage { get; }
+         public override string ToString() => GetType().Name;
+    }
+
+    public class CountryDiscountService : DiscountService
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountService(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override int DiscountPercentage
+        {
+            get
+            {
+                switch (_countryIdentifier)
+                {
+                    case "PL":
+                        return 99;
+                    default:
+                        return 5;
+                }
+            }
+        }
+    }
+
+    public class CodeDiscountService : DiscountService
+    {
+        public override int DiscountPercentage
+        {
+            get => 20;
+        }
+    }
+
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService();
+        }
+    }
+    
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        public string _code { get; set; }
+
+        public CountryDiscountFactory(string code)
+        {
+            _code = code;
+        }
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_code);
+        }
+    }
+}
--- DesignPattern/DesignPatterns/Factory/Program.cs
+++ DesignPattern/DesignPatterns/Factory/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace Factory
+﻿using System;
+using System.Collections.Generic;
+
+namespace Factory
 {
     internal class Program
     {
         public static void Main(string[] args)
-        { 
+        {
+            var factories = new List<DiscountFactory>
+            {
+                new CodeDiscountFactory(),
+                new CountryDiscountFactory("PL"),
+                new CountryDiscountFactory("asd")
+            };
+
+            foreach (var factory in factories)
+            {
+                Console.WriteLine($"Percentage: {factory.CreateDiscountService().DiscountPercentage}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
