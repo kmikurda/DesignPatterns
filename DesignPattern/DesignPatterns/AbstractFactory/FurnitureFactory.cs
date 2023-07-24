089c93977125c04dd6f240b657743acbaa0943bc
kmikurda@abis.krakow.pl <kmikurda@abis.krakow.pl>
2023-07-24 08:09:33 +0200

    abstract Factory

---
+++ DesignPattern/DesignPatterns/AbstractFactory/FurnitureFactory.cs
@@ -0,0 +1,78 @@
+﻿namespace AbstractFactory;
+
+public interface IFurnitureFactory
+{
+    public IChair CreateChair();
+    public ITable CreateTable();
+}
+
+public class ModernFurnitureFactory : IFurnitureFactory
+{
+    public IChair CreateChair()
+    {
+        return new ModernChair();
+    }
+
+    public ITable CreateTable()
+    {
+        return new ModernTable();
+    }
+}
+
+public class MedievalFurnitureFactory : IFurnitureFactory
+{
+    public IChair CreateChair()
+    {
+        return new MedievalChair();
+    }
+
+    public ITable CreateTable()
+    {
+        return new MedievalTable();
+    }
+}
+
+public interface IChair
+{
+    string getFurnitureUsage();
+}
+
+public interface ITable
+{
+    string getFurnitureUsage();
+}
+
+public class ModernChair: IChair
+{
+    public string getFurnitureUsage()
+    {
+        return "For Modern Sitting";
+    }
+}
+
+public class MedievalChair : IChair
+{
+    public string getFurnitureUsage()
+    {
+        return "For Medieval sitting";
+    }
+}
+
+public class ModernTable : ITable
+{
+    public string getFurnitureUsage()
+    {
+        return "For Modern Eating";
+    }
+}
+
+public class MedievalTable : ITable
+{
+    public string getFurnitureUsage()
+    {
+        return "For Medieval Eating";
+    }
+}
+
+
+
---
+++ DesignPattern/DesignPatterns/AbstractFactory/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AbstractFactory;
+
+Console.WriteLine("Hello, World!");
+
+var ModernFactory = new ModernFurnitureFactory();
+var ModerChair = ModernFactory.CreateChair();
+var modernTable = ModernFactory.CreateTable();
+
+var medievalFactory = new MedievalFurnitureFactory();
+var medievalChair = medievalFactory.CreateChair();
+var medievalTable = medievalFactory.CreateTable();
+
+Console.WriteLine(ModerChair.getFurnitureUsage());
+Console.WriteLine(modernTable.getFurnitureUsage());
+Console.WriteLine(medievalChair.getFurnitureUsage());
+Console.WriteLine(medievalTable.getFurnitureUsage());
